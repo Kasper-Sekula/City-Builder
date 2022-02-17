9c19e911997365fe72646bb31abb4e8bb48540e5
Kasper Sekuła <kasper.sekula@gmail.com>
2022-02-17 17:05:36 +0100

    Added mouse cursor position detection.

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Selector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera cam;
+
+    [SerializeField] static Selector instance;
+
+    void Awake()
     {
-        
+        instance = this;    
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        cam = Camera.main;    
+    }
+
+    public Vector3 GetCurTilePosition()
+    {
+        // Checking if cursor is on UI
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return new Vector3(0, -99, 9);
+        }
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        float rayOut = 0.0f;
+
+        if (plane.Raycast(ray, out rayOut))
+        {
+            Vector3 newPos = ray.GetPoint(rayOut) - new Vector3(0.5f, 0.0f, 0.5f);
+            // Rounding up
+            newPos = new Vector3(Mathf.CeilToInt(newPos.x), 0.0f, Mathf.CeilToInt(newPos.z));
+            print(newPos);
+            return newPos;
+        }
+
+        return new Vector3(0, -99, 0);
     }
 }
